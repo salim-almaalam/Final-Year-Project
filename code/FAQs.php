18ddcd8f2c1451273001a0d330f4898b6d04bfa4
Salim AL-Maalam <Salim.almaalam@gmail.com>
2020-03-01 19:07:44 +0400

    Faq edited

--- code/FAQs.php
+++ code/FAQs.php
@@ -40,101 +40,24 @@
   <h1 class="title">Departmental portal for computer science</h1>
     Frequent Asked questions here
 
-    <!-- <div class="container">
-    
-    <div class="add-Question">
-        <form class="form">
-          <input type="text" class="title" placeholder="UR question" /> <br />
-          
-          <button id="button" onClick= "change()"></button>
-        </form>
-      </div>
-    <!-- <button id ="button" >Click on me</button> 
-    <p id="para" >Hello</p>
-    </div>
-
-    <script >
-      function change(){
-        const button = document.getElementById("button");
-        const para = document.getElementById("para");
-
-      if(para.style.display = "block"){
-        para.style.display = "none";
-      }
-      else{
-        para.style.display = "block";
-      } 
-      }
-    
-    </script> -->
-
+   
 
 
 
 <button onclick="myFunction()">Toggle</button>
 
 <div id="mydiv">
-<ul>
-  <li>Hi</li>
-  <li>yes</li>
-  <li>no</li>
-  <li>wee</li>
-</ul>
+<h2>Have you got any question in UR head?</h2>
+<input type="text" id="question" placeholder="Type here...">
+<span class="Add-Q" onclick="newQ()"> <button> ADD </button>  </span>
 </div>
 
-<p><b>Note:</b> The element will not take up any space when the display property set to "none".</p>
-
-<script>
-function myFunction() {
-  const mydiv = document.getElementById("mydiv");
-  if (mydiv.style.display === "none") {
-    mydiv.style.display = "block";
-  } else {
-    mydiv.style.display = "none";
-  }
-}
-</script>
+<ul id="myList">
+  
+</ul>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <!-- <div class="container">
-      <div class="Add-question">
-        <form action="FAQs.php" method="POST">
-        <label for="question"> <strong> Type Here... </strong>  </label><br>
-        <textarea id ="question" placeholder="Add Question" name="q"> </textarea>
-        <br>
-        <input type="submit" name="submit" class="submit">
-        </form>
-      </div>
-
-      <div class="list">
-        <ul>
-          <li class="item"><a href="./apply.php"> Apply </a></li>
-          <li class="item"><a href="./courses.php"> Programmes </a></li>
-          <li class="item"><a href="./Dates.php"> Open days Dates </a></li>
-          <li class="item"><a href="./activities.php"> Activities </a></li>
-          <li class="item"><a href="./FAQs.php"> FAQs </a></li>
-          <li class="item"><a href="./register.php"> register </a></li>
-
-        </ul>
-      </div>
-      </div>
-       -->
+       <script src="./Faqs.js"></script>
   </body>
 </html>
---
+++ code/Faqs.js
@@ -0,0 +1,24 @@
+function newQ() {
+  // select items first
+  const li = document.createElement("li");
+  const question = document.getElementById("question").value;
+  const text = document.createTextNode(question);
+  li.appendChild(text);
+  if (question === "") {
+    console.log("you must input something");
+  } else {
+    document.getElementById("myList").appendChild(li); // add the question to the list
+  }
+  document.getElementById("question").value = ""; // set the input field to empty string after submitting
+
+  // console.log(question);
+}
+// question.addEventListener("keyU");
+function myFunction() {
+  const mydiv = document.getElementById("mydiv");
+  if (mydiv.style.display === "none") {
+    mydiv.style.display = "block";
+  } else {
+    mydiv.style.display = "none";
+  }
+}
